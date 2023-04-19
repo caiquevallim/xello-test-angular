d3003f7be9713060a411a9f1bf9462c2b72136b2
cvallim@getontop.com <cvallim@getontop.com>
2023-04-19 16:05:13 -0300

    included HttpClientTestingModule into product list component testing

--- src/app/product-list/product-list.component.spec.ts
+++ src/app/product-list/product-list.component.spec.ts
@@ -2,6 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ProductListComponent } from './product-list.component';
 import {PricePipe} from '../pipes/price/price.pipe';
+import {HttpClientTestingModule} from '@angular/common/http/testing';
 
 describe('ProductListComponent', () => {
   let component: ProductListComponent;
@@ -9,6 +10,7 @@ describe('ProductListComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
       declarations: [ ProductListComponent, PricePipe ]
     })
     .compileComponents();
